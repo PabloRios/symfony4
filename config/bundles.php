3eb10abaf7c06d2ecd80eb7a64e6d0f937f29cad
riospablo <PabloRios@users.noreply.github.com>
2018-02-21 21:57:29 -0300

    Agregado profiler

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     App\Application\Sonata\UserBundle\ApplicationSonataUserBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
